8da39dec19bcf09ba0f6b8127daa788f5a944d3e
Utheron <aaron.khochareun@gmail.com>
2021-04-11 11:40:40 +0200

    dev: Vue card-members created

---
+++ resources/js/components/dashboard/CardMembers.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="col hcf-admin__card ml-0">
+        <div class="hcf-admin__card-title">
+            Members
+        </div>
+        <div class="hcf-admin__card-body">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae vel, quasi reiciendis consequuntur expedita, voluptatum aspernatur tempore ducimus ea ex voluptates nulla aliquam tenetur odit, aliquid sapiente repudiandae cumque earum.
+        </div>
+        <div class="hcf-admin__card-footer"></div>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style>
+    
+</style>
